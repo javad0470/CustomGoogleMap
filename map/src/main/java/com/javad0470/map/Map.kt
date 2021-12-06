b80b6f020b9885268721358ffa3c9989d8a24eaf
javad0470 <39891130+javad0470@users.noreply.github.com>
2021-12-06 16:10:20 +0330

    Update Map.kt

--- map/src/main/java/com/javad0470/map/Map.kt
+++ map/src/main/java/com/javad0470/map/Map.kt
@@ -116,3 +116,39 @@ fun rememberMapLifecycleObserver(
         }
     }
 }
+
+@Composable
+fun AdminClubMembershipScanScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    var scanFlag by remember {
+        mutableStateOf(false)
+    }
+
+    val compoundBarcodeView = remember {
+        CompoundBarcodeView(context).apply {
+            val capture = CaptureManager(context as Activity, this)
+            capture.initializeFromIntent(context.intent, null)
+            this.setStatusText("")
+            capture.decode()
+            this.decodeContinuous { result ->
+                if(scanFlag){
+                    return@decodeContinuous
+                }
+                scanFlag = true
+                result.text?.let { barCodeOrQr->
+                    //Do something and when you finish this something
+                    //put scanFlag = false to scan another item
+                    scanFlag = false
+                }
+                //If you don't put this scanFlag = false, it will never work again.
+                //you can put a delay over 2 seconds and then scanFlag = false to prevent multiple scanning 
+                
+            }
+        }
+    }
+
+    AndroidView(
+        modifier = Modifier,
+        factory = { compoundBarcodeView },
+    )
+}
